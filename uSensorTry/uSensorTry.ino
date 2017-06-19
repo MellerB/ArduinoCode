9ceaaec7a96c034761a48bdedb19a05a0bf71e04
MellerB <mellerbartomiej@gmail.com>
2017-06-20 00:14:22 +0200

    Add files via upload

---
+++ uSensorTry/uSensorTry.ino
@@ -0,0 +1,40 @@
+#include <ST_HW_HC_SR04.h>
+int old=0;
+int act=0;
+ST_HW_HC_SR04 uSensor(13, 12); // ST_HW_HC_SR04(TRIG, ECHO)
+
+void setup() {
+    Serial.begin(9600);
+
+}
+
+int makeSense(ST_HW_HC_SR04 S)
+{
+  int tmp=(S.getHitTime()-200)/200;
+  if (tmp>8)
+  tmp=8;
+  return tmp;
+}
+
+
+
+int val;
+String mess;
+void loop() {
+    mess="";
+    act=makeSense(uSensor);
+    if(act!=-1)
+    {
+    val=act;
+    old=act;
+    }
+    else
+    val=old;
+    
+    for(int i=0;i<val;i++)
+    mess=mess+"XXXXXXXX";
+    
+    Serial.println(mess);
+    
+
+}
